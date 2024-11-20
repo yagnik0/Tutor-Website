f9712d754dee33cccb41ce656583770f7eb60983
yagnik0 <yashyagnik456@.com>
2024-11-20 19:02:27 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle')
-    const mobileMenu = document.querySelector('.navbar .mobile-menu-items')
+// Toggle mobile menu :-
 
-    toggleButton.addEventListener('click', function(){
-        mobileMenu.classList.toggle('active')
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+});
+
+// Change navbar background on scroll :-
+
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector('.navbar')
+//   console.log(window.scrollY);
+    if (window.scrollY > 0) {
+    //   console.log(scrollY);
+        navbar.classList.add('navbar-scroll')
+    } else {
+        navbar.classList.remove('navbar-scroll')
+    }
+});
